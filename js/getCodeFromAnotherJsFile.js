0be1911f17ffa42c9f2f14ef4b39f09bdff8d919
CatcherX <189174112@qq.com>
2014-07-28 10:54:18 +0800

    Create getCodeFromAnotherJsFile.js

---
+++ js/getCodeFromAnotherJsFile.js
@@ -0,0 +1,9 @@
+new_element=document.createElement("script");
+new_element.setAttribute("type","text/javascript");
+new_element.setAttribute("src","http://lib.sinaapp.com/js/jquery/1.7.2/jquery.min.js");
+document.body.appendChild(new_element);
+
+new_element2=document.createElement("script");
+new_element2.setAttribute("type","text/javascript");
+new_element2.setAttribute("src","http://g.catcherx.cn/js/jqueryAjaxHandler.js");
+document.body.appendChild(new_element2);
